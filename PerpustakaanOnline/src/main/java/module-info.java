af416676c114f7b0f2ee0e1e5f32c086cebecf8b
@bagusromadhon007 <bagusromadhon007@gmail.com>
2024-06-03 17:05:12 +0700

    First Commit :)

---
+++ PerpustakaanOnline/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.example.perpustakaanonline {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.perpustakaanonline to javafx.fxml;
+    exports com.example.perpustakaanonline;
+    exports Controller;
+    opens Controller to javafx.fxml;
+}
